f48c4b4dacde1edade0adee7b3b8a64966c6fff5
DeiNas77 <deinas.44remastered@gmail.com>
2026-09-03 16:35:33 -0500

    GKCB-01 | feat: add global repositories pattern (buildRepositories)

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import type { Express } from "express";
 
 import { AppDataSource } from "./database/appDataSource.js";
+import { buildRepositories } from "./database/repositories/globalRepositories.js";
 
 async function main() {
   try {
     await AppDataSource.initialize();
+    buildRepositories(AppDataSource);
     const imported = await import("./app.js");
     const app = imported.default as Express;
     const port: number = app.get("port") as number;
